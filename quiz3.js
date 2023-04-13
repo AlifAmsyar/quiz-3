955a88c13e6c4e01810312a6a649fb73a819607d
Alif Amsyar <maamsyar00@gmail.com>
2023-04-13 15:54:40 +0800

    quiz3.js

--- quiz3.js
+++ quiz3.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const jwt = require('jsonwebtoken')
 
 app.use(express.json())
 
@@ -9,14 +10,15 @@ app.post('/login', (req, res) => {
 
     let result = login(req.body.username, req.body.password)
 
-    res.send(result)
+    let Token = generateToken(result)
+    res.send(Token)
 })
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/bye', (req, res) => {
+app.get('/bye', verifyToken, (req, res) => {
     res.send('Bubyeee Semua!')
 })
 
@@ -34,12 +36,16 @@ app.post('/register', (req, res) => {
     res.send(Result)
 })
 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
 let dbUsers = [
   {
-    username: "Alif",
+    username: "Amsyar",
     password: "1234",
     name: "Alif",
-    email: "Alif@utem.edu.my"
+    email: "Amsyar@utem.edu.my"
   }
 ]
 
@@ -66,8 +72,28 @@ function register(requsername, reqpassword, reqname, reqemail) {
   })
 }
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+function generateToken(userData)
+{
+  const Token = jwt.sign
+  (
+    userData,
+    'inipassword',
+    {expiresIn: 60}
+  );
+  return Token
+}
 
-//try login
+function verifyToken (req,res,next){
+  let header = req.headers.authorization
+  console.log (header)
+
+  let Token = header.split('')[1]
+
+  jwt.verify(Token, 'inipassword',function(err,decoded){
+    if(err){
+      res.send("Invalid Token")
+    }
+    req.user = decoded
+    next()
+  });
+}
